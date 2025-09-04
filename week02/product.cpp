8fc532d39b5ef73af69e01c784d7ff7d313ccd56
HelloCoding32 <user.email>
2025-09-04 11:15:25 +0900

    .gitignore & *.h *.cpp

---
+++ week02/product.cpp
@@ -0,0 +1,15 @@
+#include "product.h"
+
+Product::Product(string nm, double up)
+	: name(nm), unitPrice(up)
+{
+}
+
+Product::~Product()
+{
+}
+
+double Product::getPrice() const
+{
+	return unitPrice;
+}
