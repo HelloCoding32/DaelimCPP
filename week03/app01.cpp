4e25f324b6d139149e160b4fa9be3d0075cec1f2
HelloCoding32 <user.email>
2025-09-11 12:20:29 +0900

    v0.4

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -8,12 +8,16 @@ using namespace std;
 class Pokemon
 {
 public:
+	Pokemon() { cout << "Default(Pokemon) constructor\n";}
+	virtual ~Pokemon() { cout << "Base class (Pokemon) destructor\n"; } //prevent memory leak
 	virtual void attack() const { cout << "Attack!" << endl; }
 };
 
 class Pikachu : public Pokemon // »ó¼Ó inheritance is-a
 {
 public:
+	Pikachu() { cout << "Default(Pikachu) constructor\n"; }
+	~Pikachu(){ cout << "Derived class (Pikachu) destructor\n"; }
 	void attack() const { cout << "Electric Attack" << endl; }
 };
 int main()
