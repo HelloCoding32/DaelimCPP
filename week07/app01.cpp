607d54e51963bbd3b1b04f0c23ce14d2955c67ef
HelloCoding32 <feeling0706@naver.com>
2025-10-02 11:36:13 +0900

    go

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Person {
 public:
     string name;
-    // virtual void warn() = 0;
+    //virtual void warn() = 0;
 };
 class UndergraduateStudent : virtual public Person{
 public:
@@ -19,15 +19,14 @@ public:
 class UndergraduateDormitoryStudent
     : public UndergraduateStudent, public DormitoryStudent{
 };
-
 int main() {
     UndergraduateDormitoryStudent uds;
     //uds.warn();  // 다중상속시 어느 부모의 warn함수를 호출할지 모호함
     uds.DormitoryStudent::warn();
     uds.gpa = 3.9;
     uds.roomNumber = 201;
-    uds.name = "Kim";  // virtual 키워드로 양쪽 부모클래스가 할아버지 클래스를 상속받을 때 Virtual 키워드를 이용해서 죽음의 다이아몬드 문제를 해결
+    uds.name = "Kim";  // 양쪽 부모클래스가 할아버지 클래스를 상속받을 때 virtual 키워드를 이용해서 죽음의 다이아몬드 문제를 해결
 
-    cout << uds.name << "의 평점은 "<< uds.gpa << "이고, 기숙사 방번호는 " << uds.roomNumber <<"호 입니다\n" << endl;
+    cout << uds.name << "의 평점은 " << uds.gpa << "이고, 기숙사 방번호는 " << uds.roomNumber << "호 입니다\n";
     return 0;
 }
