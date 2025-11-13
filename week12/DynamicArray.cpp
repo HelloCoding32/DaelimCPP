062ae2b953473a3b95c4773abf066a6b2e1ac680
HelloCoding32 <feeling0706@naver.com>
2025-11-13 10:55:26 +0900

    exception make custom exception fold

--- week12/DynamicArray.cpp
+++ week12/DynamicArray.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "../week11/DynamicArray.h"
+#include "DynamicArray.h"
+#include "MyException.h"
 using namespace std;
 
 DynamicArray::DynamicArray()
@@ -12,7 +13,7 @@ DynamicArray::DynamicArray(int size) : size(size)
         //this->size = size;
         ptr = new int[size];
         cout << "동적배열 생성됨\n";
-        throw "4885\n";
+        throw "메모리 오류 발생\n";
     }
     catch (...) {
         delete[] ptr;
@@ -31,13 +32,13 @@ DynamicArray::~DynamicArray()
 int DynamicArray::getAt(int index)
 {
     if (index >= size || index < 0)
-        throw 1;
+        throw MyException(4885, "인덱스 범위를 벗어났습니다\n", this);
     return ptr[index];
 }
 
 void DynamicArray::setAt(int index, int value)
 {
     if (index >= size || index < 0)
-        throw 0;
+        throw false;
     ptr[index] = value;
 }
---
+++ week12/MyException.h
@@ -0,0 +1,7 @@
+#pragma once
+class MyException {
+private:
+   int errorCode;
+   const char* errorMessage;
+   const MyException* errorAddress;
+};
