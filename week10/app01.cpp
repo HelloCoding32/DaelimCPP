bbfcff761bef754bfe58d0f927f6617157ce3dac
HelloCoding32 <feeling0706@naver.com>
2025-11-13 10:08:25 +0900

    all of everything

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -1,10 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // double c = 3 * 7 * 2.2; // 묵시적 형변환
-    int c = 3 * 7 * 2.2;
+class Complex {
+private:
+    int real;
+    int imaginary;
+public:
+    Complex() : real(0), imaginary() {
 
-    cout << c << endl;
+    };
+    Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
+
+    };
+    void setReal(int real) { //생성자가 아니여서 초기화 리스트를 쓸 수 없다.
+        this->real = real;
+    }
+    int getReal() const {
+        return real;
+    }
+    void setimaginary(int imaginary) { //생성자가 아니여서 초기화 리스트를 쓸 수 없다.
+        this->imaginary = imaginary;
+    }
+    int getimaginary() const {
+        return imaginary;
+    }
+
+    // member function (method)
+    // Complex operator+(const Complex& right)  { //operator => 연산자 오버로딩
+    //     int r = this->real + right.real;
+    //     int imag = this->imaginary + right.imaginary;
+    //     return Complex(r, imag);
+    //     // return Complex(r, this-> iamginary) //가능하나 수학적으로 문제가 됨.
+    // }
+    Complex operator++(int) { // int => Tag, 후위연산
+        Complex previous (this->real, this->imaginary);
+        this->real = this->real + 1;
+        return previous;
+    }
+    Complex operator++() { // 전위연산
+        this->real++;
+        return Complex(this->real, this->imaginary);
+    }
+    friend ostream& operator<<(ostream& o, const Complex& right) { //complex& 뒤에 &의 의미는 얕은 복사임, complex만 쓰면 깊은 복사로 쓰임.
+        o << right.real << "+" << right.imaginary << "i" << endl;
+        return o;
+    }
+
+};
+
+
+// nonmember function
+
+istream& operator>>(istream& i, Complex& right) {
+    cout << "input real value : ";
+    int real, img;
+    i >> real;
+    right.setReal(real);
+    cout << "input imaginary value : ";
+    i >> img;
+    right.setimaginary(img);
+    return i;
+
+}
+
+Complex operator+(const Complex& left, const Complex& right)  { //operator => 연산자 오버로딩
+    int r = left.getReal() + right.getReal();
+    int imag = left.getimaginary() + right.getimaginary();
+    return Complex(r, imag);
+}
+
+Complex operator+(int left, const Complex& right) {
+    return Complex(left + right.getReal(), right.getimaginary());
+}
+
+int main() {
+    Complex c1(10, 7);
+    int x =5;
+    cout << c1;
+    Complex c2 = (x + c1);
+    cout << c2 <<endl; // 15 + 7i
     return 0;
 }
---
+++ week11/DynamicArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "DynamicArray.h"
+using namespace std;
+
+DynamicArray::DynamicArray()
+{
+}
+
+DynamicArray::DynamicArray(int size) : size(size)
+{
+    try {
+        //this->size = size;
+        ptr = new int[size];
+        cout << "동적배열 생성됨\n";
+        throw "4885\n";
+    }
+    catch (...) {
+        delete[] ptr;
+        cout << "힙메모리 해제 (생성자 안쪽)\n";
+
+        throw;
+    }
+}
+
+DynamicArray::~DynamicArray()
+{
+    cout << "동적배열 메모리 해제\n";
+    delete[] ptr;
+}
+
+int DynamicArray::getAt(int index)
+{
+    if (index >= size || index < 0)
+        throw 1;
+    return ptr[index];
+}
+
+void DynamicArray::setAt(int index, int value)
+{
+    if (index >= size || index < 0)
+        throw 0;
+    ptr[index] = value;
+}
